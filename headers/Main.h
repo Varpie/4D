05f9466dbe920c970df1d70c41fb2bb6e3ebebbb
Pierre V <pierre.varlez@gmail.com>
2016-02-21 12:41:32 +0100

    Putting every #includes needed, removing useless stuff.

--- headers/Main.h
+++ headers/Main.h
@@ -1,16 +1,15 @@
 // No copyrights, it's free.
-#ifndef _Main_H
-#define _Main_H
+#pragma once
 
-#include <iostream>
-#include <SDL2/SDL.h>
-#define GLEW_STATIC
 #include <GL/glew.h>
-#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
 
 int SCREEN_WIDTH = 1920;
 int SCREEN_HEIGHT = 1080;
-
-void close();
-
-#endif
